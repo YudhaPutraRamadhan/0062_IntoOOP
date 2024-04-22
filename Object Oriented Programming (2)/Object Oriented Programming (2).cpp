ebe88fb5c3379fbace983c3addd0d80c198b3366
MzSuwawa <yudharamadhan838@gmail.com>
2024-04-22 16:19:18 +0700

    Menambahkan prosedur input untuk bangun datar

--- Object Oriented Programming (2)/Object Oriented Programming (2).cpp
+++ Object Oriented Programming (2)/Object Oriented Programming (2).cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 class bangunDatar {
-	private:
-		float panjang, Lebar;
-	public:
-		float Luas;
-};
+private:
+	float panjang, Lebar;
+public:
+	float Luas;
+
+	void input() {
+		cout << "Masukkan Panjangnya = ";
+		cin >> panjang;
+		cout << "Masukkan Lebarnya = ";
+		cin >> Lebar;
+	}
+}
